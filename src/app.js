2557dcf7600a856e34149a84bfc3bab5bad119c2
sharexy23 <omotietie@yahoo.com>
2024-01-03 20:37:38 +0100

    it works its just regular changes now

--- src/app.js
+++ src/app.js
@@ -6,81 +6,125 @@ const puppeteer = require('puppeteer');
 
 const app = express();
 
-// if (config.env !== 'test') {
-//   app.use(morgan.successHandler);
-//   app.use(morgan.errorHandler);
-// }
-// parse json request body
 app.use(express.json());
 
-// parse urlencoded request body
-app.use(express.urlencoded({ extended: true }));
+let caption = ""
+const numberOfPostPerAccount = 5
+let currentIndex = 0; // Index for cycling through accounts
+let iterationsCount = 0;
+const localFilePath = 'downloaded-file.jpg';
 
-// const filePath = '/Users/sharonowolabi/Desktop/web_automation_IG/src/accounts.txt';
+const filePath = 'src/accounts.txt';
+const accounts = fileWorks.getAccountsFromFile(filePath);
+console.log(accounts);
 
-// // Read file content
-// const fileContent = fileWorks.readFile(filePath);
-
-// if (fileContent) {
-//   // Get lines from file content
-//   const lines = fileWorks.getLines(fileContent);
-//   console.log(lines);
-
-//   // Get accounts from file
-//   const accounts = fileWorks.getAccountsFromFile(filePath);
-//   console.log(accounts);
-// }
-
-///reddit 
-// const subreddit = 'memes'; // You can change this to any subreddit of your choice
-//   console.log("i work");
-//   redditApi.getRandomMemeWithTopComment(subreddit).then(({ memeUrl, topComment }) => {
-//     console.log("i work 2");
-//     if (memeUrl) {
-//       console.log("i work3");
-//     console.log('Download your meme:', memeUrl);
-//     console.log('Top Comment:', topComment);
-//     // Implement the logic to download and save the meme to your laptop
-//   } else {
-//     console.log('Failed to fetch meme with top comment.');
-//   }
-// });
-app.get('/', async (req, res) => {
+async function performLoginAndRecurringActions(account) {
   const browser = await puppeteer.launch({
     args: ["--no-sandbox", "--disable-setuid-sandbox"],
+    headless: false
   });
-  // Create a new page
   const page = await browser.newPage();
 
-  // Replace these with your actual credentials and login URL
-  const username = 'your_username';
-  const password = 'your_password';
-  const loginUrl = 'https://www.example.com/';
   try {
+    await page.goto('https://www.instagram.com/accounts/login/');
 
-    // Navigate to the login page
-    await page.goto(loginUrl);
-    console.log("i work");
     await page.waitForSelector('input[name=username]');
-    console.log("i work");
-    await page.type('input[name=username]', username); 
-    await page.type('input[name=password]', password); 
-    await page.click('button[type=submit]'); 
-   
-    
+
+    await page.type('input[name=username]', account.username);
+    await page.type('input[name=password]', account.password);
+    await page.click('button[type=submit]');
+
+    // // Wait for navigation or any other action after login (replace with specific waiting conditions)
     await page.waitForSelector('svg[aria-label="New post"]'); 
     
     console.log('Successfully logged in!');
 
-    // You can add further actions after login if needed
+    const recurringAction = () => {
+        console.log("i work");
+        redditApi.getRandomMemeWithTopComment().then(({ memeUrl, topComment, title }) => {
+          console.log("i work 2");
+          if (memeUrl) {
+            console.log(memeUrl, topComment,title );
+            redditApi.downloadAndUploadFile(memeUrl, localFilePath)
+            caption = topComment || title
+          } else {
+            console.log('Failed to fetch meme with top comment.');
+          }
+        });
+        postMeme(page, caption)
+      iterationsCount++;
 
-    // Send a response to the client
-    res.send('Successfully logged in!');
+      if (iterationsCount >= numberOfPostPerAccount) {
+        iterationsCount = 0; // Reset the counter
+        currentIndex = (currentIndex + 1) % accounts.length; // Move to the next account
 
+        // Stop recurring actions for the current account
+        clearInterval(intervalId);
+        console.log(`Stopped recurring actions for account ${account.username}`);
+        cycleThroughAccounts()
+      }
+    };
+
+    recurringAction()
+    // Set an interval for the recurring actions (every 30-50 minutes in this example)
+    const intervalId = setInterval(recurringAction, 5 * 60000); // Random interval between 30-50 minutes
+    
   } finally {
     // Close the browser
-    await browser.close();
+    //await browser.close();
   }
+}
+
+
+async function postMeme(page, caption) {
+  
+  try {
+    await page.click('svg[aria-label="New post"]');
+    await page.waitForSelector('.x9f619.xjbqb8w.x78zum5.x168nmei.x13lgxp2.x5pf9jr.xo71vjh.x1iorvi4.x150jy0e.xjkvuk6.x1e558r4.x1n2onr6.x1plvlek.xryxfnj.x1c4vz4f.x2lah0s.xdt5ytf.xqjyukv.x1qjc9v5.x1oa3qoh.x1nhvcw1'); 
+
+    const [fileChooser] = await Promise.all([
+      page.waitForFileChooser(),
+      page.click('.x9f619.xjbqb8w.x78zum5.x168nmei.x13lgxp2.x5pf9jr.xo71vjh.x1iorvi4.x150jy0e.xjkvuk6.x1e558r4.x1n2onr6.x1plvlek.xryxfnj.x1c4vz4f.x2lah0s.xdt5ytf.xqjyukv.x1qjc9v5.x1oa3qoh.x1nhvcw1'), // some button that triggers file selection
+    ]);
+
+    await fileChooser.accept(['downloaded-file.jpg'])
+    const nextSelector = ".x9f619.xjbqb8w.x78zum5.x168nmei.x13lgxp2.x5pf9jr.xo71vjh.xyamay9.x1pi30zi.x1l90r2v.x1swvt13.x1n2onr6.x1plvlek.xryxfnj.x1c4vz4f.x2lah0s.xdt5ytf.xqjyukv.x1qjc9v5.x1oa3qoh.x1nhvcw1"
+    await page.waitForSelector(nextSelector); 
+    console.log("got it");
+    await page.click(nextSelector);
+
+    await page.waitForSelector(nextSelector); 
+    console.log("got it");
+    await page.click(nextSelector);
+
+    await page.waitForSelector('div[role=textbox]');
+    console.log("find it, found");
+    await page.type('div[role=textbox]', caption);
+
+
+    await page.waitForSelector(nextSelector);
+    console.log('Successfully logged in!');
+    await page.click(nextSelector);
+  } catch (error) {
+    console.log(error);
+  }
+
+}
+
+
+
+// parse urlencoded request body
+app.use(express.urlencoded({ extended: true }));
+
+function cycleThroughAccounts() {
+  const currentAccount = accounts[currentIndex];
+  performLoginAndRecurringActions(currentAccount);
+}
+
+
+app.get('/', async (req, res) => {
+  const currentAccount = accounts[currentIndex];
+  performLoginAndRecurringActions(currentAccount);
 });
 
 const port = process.env.PORT || 4000
--- src/utils/redditApi.js
+++ src/utils/redditApi.js
@@ -1,4 +1,6 @@
 const snoowrap = require('snoowrap');
+const fs = require('fs');
+const axios = require('axios');
 
 const reddit = new snoowrap({
   userAgent: 'web automation',
@@ -8,22 +10,63 @@ const reddit = new snoowrap({
   password: '@#Phone123',
 });
 
-async function getRandomMemeWithTopComment(subreddit) {
+const subredditList = ['FunnyMemePics'];
+
+// Function to select a random item from the list
+function getRandomItem(list) {
+  const randomIndex = Math.floor(Math.random() * list.length);
+  return list[randomIndex];
+}
+
+// Select a random item from the list
+const randomItem = getRandomItem(subredditList);
+
+async function getRandomMemeWithTopComment() {
     try {
         console.log("i work here");
-        const posts = await reddit.getSubreddit(subreddit).getTop({ time: 'month', limit: 10 });
-        console.log("i work here 2");
-    const randomPost = posts[Math.floor(Math.random() * posts.length)];
-    console.log(randomPost);
-    // Get the top comment for the selected post
-    const comments = await randomPost.comments.fetchAll({ limit: 1 });
-    const topComment = comments.length > 0 ? comments[0].body : 'No comments available';
-    console.log("i work here 4");
-    return { memeUrl: randomPost.url, topComment };
+        const posts = await reddit.getSubreddit(randomItem).getTop({ time: 'month', limit: 20 });
+      console.log("i work here 2");
+      const jpegPosts = posts.filter(post => post.url.endsWith('.jpeg'));
+      if (jpegPosts.length > 0) {
+        const randomPost = jpegPosts[Math.floor(Math.random() * jpegPosts.length)];
+        console.log(randomPost.url, "post url");
+        // Get the top comment for the selected post
+        const comments = await randomPost.comments.fetchAll({ limit: 1 });
+        const topComment = comments.length > 0 ? comments[0].body : 'No comments available';
+        console.log("i work here 4");
+        return { memeUrl: randomPost.url, topComment, title: randomPost.title };
+      } else {
+        return { memeUrl: "https://i.redd.it/x38c1c8mxlv21.jpg", topComment: "404 for real", title: "404 for real" };
+      }
+    
   } catch (error) {
     console.error('Error fetching meme with top comment:', error.message);
     return null;
   }
 }
 
-module.exports = { getRandomMemeWithTopComment };
+const downloadAndUploadFile = async (url, destinationPath) => {
+    // Use axios to make a GET request to the specified URL with responseType set to 'stream'
+    const response = await axios({
+      url,
+      method: 'GET',
+      responseType: 'stream',
+    });
+    console.log("downlord works");
+    // Create a writable stream to the specified destinationPath
+    const writer = fs.createWriteStream(destinationPath);
+    console.log("downlord work");
+    // Return a Promise that resolves when the file has finished writing
+    return new Promise((resolve, reject) => {
+      response.data.pipe(writer);
+  
+      // The 'finish' event is emitted when writing is complete
+      writer.on('finish', resolve);
+  
+      // The 'error' event is emitted if there is an issue with the writing process
+      writer.on('error', reject);
+    });
+  };
+  
+
+module.exports = { getRandomMemeWithTopComment, downloadAndUploadFile };
